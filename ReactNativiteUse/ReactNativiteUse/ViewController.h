eaf067d908fe63fb9ee168cc98b79ae5e747fff0
Goyohol <192200968@qq.com>
2018-04-19 20:52:21 +0800

    md文档 书写

---
+++ ReactNativiteUse/ReactNativiteUse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactNativiteUse
+//
+//  Created by RainHeroic Kung on 2018/4/19.
+//  Copyright © 2018年 RainHeroic Kung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReactNativiteUse/ReactNativiteUse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactNativiteUse
+//
+//  Created by RainHeroic Kung on 2018/4/19.
+//  Copyright © 2018年 RainHeroic Kung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
